6b5af9dde23d8e0ad859d99a31e4bd9eae20002b
priyanshu9026 <singpriyasnshu22@gmail.com>
2024-04-28 01:53:05 +0000

    initial commit

--- src/components/FailureView/styledComponents.js
+++ src/components/FailureView/styledComponents.js
@@ -13,6 +13,27 @@ export const FailedImage = styled.img`
     width: 450px;
   }
 `
-export const FailedHeading = styled.
-export const FailedNote = styled.
-export const RetryButton = styled.
+export const FailedHeading = styled.h1`
+  font-family: 'Roboto';
+  font-size: 25px;
+  color: ${props => props.headingColor};
+  text-align: center;
+`
+export const FailedNote = styled..p`
+  font-family: 'Roboto';
+  font-size: 18px;
+  color: ${props => props.noteColor};
+  text-align: center; 
+`
+export const RetryButton = styled.button`
+  border: none;
+  background-color: #4f46e5;
+  border-radius: 3px;
+  color: #ffffff;
+  padding-left : 10px;
+  padding-right : 10px;
+  padding-top : 5px;
+  padding-bottom : 5px;
+  font-family: 'Roboto';
+  font-size: 18px;
+`
